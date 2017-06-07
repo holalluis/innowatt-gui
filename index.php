625007e58bc827c410ae831ab7438c178afbe59c
Lluís Bosch <lbosch@icra.cat>
2017-06-07 20:07:01 +0200

    segon commit

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-
 <!doctype html><html> <head>
 	<meta charset=utf-8>
 	<title>innoWatt</title>
 </head><body>
 
+<?php include'navbar.php'?>
+
 <h1>innoWatt</h1>
 
-<ul id=navbar>
-	<li><a href=index.php>Home</a>
-	<li><a href=corba.php>Corba horària (kW)</a>
-</ul>
+pàgina principal
 
-<style>
-	#navbar {
-	}
-</style>
---
+++ navbar.php
@@ -0,0 +1,15 @@
+<ul id=navbar>
+	<li><a href=index.php>Home</a>
+	<li><a href=corba.php>Corba horària (kW)</a>
+</ul>
+
+<style>
+	#navbar {
+		display:flex;
+		flex-wrap:wrap;
+	}
+	#navbar li {
+		display:block;
+		padding:0.5em;
+	}
+</style>

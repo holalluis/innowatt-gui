cfb1057ebe9438343765198e5045a981708ccba0
root <root@debian>
2017-06-07 20:03:39 +0200

    first

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!doctype html><html> <head>
+	<meta charset=utf-8>
+	<title>innoWatt</title>
+</head><body>
+
+<h1>innoWatt</h1>
+
+<ul id=navbar>
+	<li><a href=index.php>Home</a>
+	<li><a href=corba.php>Corba horària (kW)</a>
+</ul>
+
+<style>
+	#navbar {
+	}
+</style>
